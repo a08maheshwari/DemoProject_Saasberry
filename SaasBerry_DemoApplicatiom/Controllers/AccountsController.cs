fd47a85ca77632a2c75dbfda1a493cd3bc5f4ba6
Anshul_m <anshul@nestorbird.com>
2020-11-26 01:29:06 +0530

    Account Listing

---
+++ SaasBerry_DemoApplicatiom/Controllers/AccountsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DemoApp.Data.Entities.DB;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SaasBerry_DemoApplication.Controllers
+{
+    public class AccountsController : Controller
+    {
+        private dbsbltest1Entities dbContext;
+        public IActionResult AccountList()
+        {
+            var totalRec = 0;
+            var AccountList = dbContext.proc_GetAccountByPage(1,10);
+            ViewBag.Alist = AccountList;
+            ViewBag.AccountListCount = totalRec;
+            return View();
+        }
+
+    //    public ActionResult AccountLoadMore(int? accountCount)
+    //    {
+    //                    var totalRec = 0;
+           
+               
+    //            //var SearchClientListFinal = objClient.GetAccountListing();
+    //            //ViewBag.ClientListCount = totalRec;
+               
+           
+    //            //    return Json(lst, JsonRequestBehavior.AllowGet);
+               
+    //}
+}
