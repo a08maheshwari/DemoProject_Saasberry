d44f27bc472f5e1206a93866591732ab1b4a2f21
Anshul_m <anshul@nestorbird.com>
2020-11-25 22:35:34 +0530

    initial commit

---
+++ SaasBerry_DemoApplicatiom/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SaasBerry_DemoApplicatiom.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
